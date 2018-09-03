db4722843014290a6a4fa146ae2cad7534719994
Android <androidworking@mail.ru>
2018-09-03 11:50:39 +0300

    End 2, refactor

--- func.php
+++ func.php
@@ -49,12 +49,10 @@ function send_email($order_id, $number_orders, $pdo)
     $sql = $pdo->prepare($query);
     $sql->execute();
     $address_orders = $sql->fetch(PDO::FETCH_ASSOC);
-    //echo $address_orders['street']. "<br />";
     $time_order = date('d.m.Y H.i'); // фиксируем текущее время
     $file = 'letters/letter_order.html';
     $title = "Заказ № $order_id<br>";
     $time = "Время заказа - $time_order<br/>";
-    //$fullAddress = "Ваш заказ будет доставлен по адресу - ул. $zakaz['street']. , дом $home, корпус $part, квартира $appt, этаж $floor<br>";
     $text = "DarkBeefBurger за 500 рублей, 1 шт<br>";
     if ($number_orders == 1) {
         $count_order = "Спасибо - это ваш первый заказ!";
@@ -69,85 +67,3 @@ function send_email($order_id, $number_orders, $pdo)
     $write_order .= $order_message;
     file_put_contents($file, $write_order);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function authoriz_registr($name, $email, $phone, $street, $home, $appt, $floor, $comment, $part, $pdo)
-//{
-//    $authoriz = "SELECT `id` FROM `users` WHERE `email` = '$email'"; // получаем id пользователя из базы
-//    $sql = $pdo->prepare($authoriz);
-//    $sql->execute(); // запрос на выполнение
-//    $result = $sql->fetch(PDO::FETCH_ASSOC);// получаем данные (id пользователя) в виде массива
-//    if ($result === false) { // Если это новый посетитель
-//        $authoriz = "INSERT INTO `users`(`name`, `email`, `phone`) VALUES ('$name', '$email', '$phone');"; // Добавляем его в базу
-//        $sql = $pdo->prepare($authoriz);
-//        $sql->execute();
-//        $authoriz = "SELECT `id` FROM `users` WHERE `email` = '$email'";
-//        $sql = $pdo->prepare($authoriz);
-//        $sql->execute();
-//        $result = $sql->fetch(PDO::FETCH_ASSOC);
-//    } else {
-//        $authoriz = "INSERT INTO `orders`(`id_users`, `street`, `home`, `floor`, `appt`, `name`, `corp`, `comment`) VALUES ({$result['id']}, '$street', '$home', '$floor', '$appt', '$name', '$part', '$comment');";
-//        $sql = $pdo->prepare($authoriz);
-//        $sql->execute();
-//    }
-//}
-//$last_id = $pdo->lastInsertId(); // Номер заказ
-//function countOrder($result, $pdo)
-//{
-//    $query = "SELECT COUNT(*) as number_orders FROM `orders` WHERE `id_users` = {$result['id']}";
-//    $result = $pdo->prepare($query);
-//    $result->execute();
-//    $number_of_rows = $result->fetch(PDO::FETCH_ASSOC);// получаем количество заказов
-//    $cout_orders = $number_of_rows[number_orders]; // Используем переменную для прямого доступа к значению количества заказов
-//
-//    if ($cout_orders == 1) {
-//        $count_order = "Спасибо - это ваш первый заказ!";
-//
-//    } else {
-//        $count_order = "Спасибо! Это уже $cout_orders заказ!";
-//
-//    }
-//    return $count_order;
-//}
-//$last_id = $pdo->lastInsertId(); // Номер заказа
-//
-//function user() {
-//    global $pdo;
-//    $stmt = $pdo->query('SELECT * FROM users');
-//    $data = $stmt->fetchAll();
-//    return $data;
-//}
-//$datas = user();
-//var_dump($datas);
